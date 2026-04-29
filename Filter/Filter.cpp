d887c8446fa8f47e9a27e68a2c10da568bf65dc3
Muhammad Ibrahim <m.ibraheem.isb@gmail.com>
2026-04-29 23:06:15 +0500

    Add Filter class implementation
    Add Filter/Filter.cpp implementing the Filter class methods: default and parameterized constructors, getters for filter ID, name, category and enabled state, and a setter for enabled. Provides basic accessors and mutators for filter objects.

---
+++ Filter/Filter.cpp
@@ -0,0 +1,31 @@
+#include "Filter.h"
+
+// Default Constructors:
+Filter::Filter() : filterID(0), filterName(" "), Category(" "), isEnabled("False") {}
+
+// Parametrized Constructor:
+Filter::Filter(int id, string name, string category, bool enabled = true) : filterID(id), filterName(name), Category(category), isEnabled(enabled) {}
+
+// Getters:
+int Filter::getFilterID() const
+{
+  return filterID;
+}
+string Filter::getFilterName() const
+{
+  return filterName;
+}
+string Filter::getCategory() const
+{
+  return Category;
+}
+bool Filter::isFilterEnabled() const
+{
+  return isEnabled;
+}
+
+// Setter:
+void Filter::setEnabled(bool status)
+{
+  isEnabled = status;
+}
